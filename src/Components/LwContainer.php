0b1bf3c2465c34e0486cb445514c5ad69c6c3907
sitesurfer <sitesurfer@users.noreply.github.com>
2021-11-03 18:02:54 +0000

    Fix styling

--- src/Components/LwContainer.php
+++ src/Components/LwContainer.php
@@ -3,11 +3,9 @@
 namespace Sitesurfer\Lwcrud\Components;
 
 use Illuminate\View\Component;
-use Spatie\LaravelRay;
 
 class LwContainer extends Component
 {
-
     //back up vars array
     public $appData;
 
@@ -31,35 +29,30 @@ class LwContainer extends Component
     private $listData;
     public $editSettings;
 
-    public function __construct($d,$a)
+    public function __construct($d, $a)
     {
         //add variables as if this came from us directly
         $this->listData = $d;
         $this->appData = $a;
 
         //ray($this->appData);
-        foreach ($a as $key => $value)
-        {
-           $this->$key = $value;
+        foreach ($a as $key => $value) {
+            $this->$key = $value;
         }
 
-        if(!empty($this->listNames))
-        {
+        if (! empty($this->listNames)) {
             //make simple list view up internally
             $this->listView = 'lwcrud::partials.lw-list-table';
         }
 
-        if(empty($this->createView))
-        {
+        if (empty($this->createView)) {
             $this->createView = 'lwcrud::partials.lw-edit-modal';
         }
 
-        if(empty($this->listView))
-        {
+        if (empty($this->listView)) {
             //if we have neither then provide an error.
             die("No List View or Names provided, please check your configuration");
         }
-
     }
 
     public function render()
